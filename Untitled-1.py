375fd5e67da048ae689c39015859dbfb642e7521
abdullahhchh <145340077+abdullahhchh@users.noreply.github.com>
2023-09-18 15:26:33 +0500

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,27 @@
+print("hello world")
+
+print(1,2,3)
+
+# /n to start a new line 
+
+a = complex(8, 2)
+b = True
+c = "Abdullah"
+d = None
+print(a)
+print(b)
+b1 = 9
+print(a + b1)
+print("The type of a is ", type(a))
+print("The type of b is ", type(b))
+print("The type of c is ", type(c))
+
+
+list1 = [8, 2.3, [-4, 5], ["apple", "banana"]]
+print(list1)
+
+tuple1 = (("parrot", "sparrow"), ("Lion", "Tiger"))
+print(tuple1)
+
+dict1 = {"name":"Sakshi", "age":20, "canVote":True}
+print(dict1)
